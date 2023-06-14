b5b182cbb5603bc953f773e16261985c01bafb3b
JinsolKim-1 <atomicnick43@gmail.com>
2023-06-14 23:10:34 +0800

    Variables practice

---
+++ Variables practice.py
@@ -0,0 +1,26 @@
+# String
+name = "SCP-049"
+classification = "Euclid"
+height = "1.9 meters" 
+
+print ("Item number: " + (name))
+print ("class: " + (classification))
+print ("height: " + (height))
+
+# Integer
+age = 200
+age = age + 3
+print ("Age: "+ str(age) + " Years old")
+
+#Float
+weight = 92.754
+print ("SCP weight: " +str(weight) +" kg")
+
+#Boolean
+safe = True
+Euclid = True
+Keter = False
+print ("Is SCP-049 safe?: " +str(safe))
+print ("Is SCP-049 Euclid?: " +str(Euclid))
+print ("Is SCP-049 have any other classes?: " +str(Keter))
+
