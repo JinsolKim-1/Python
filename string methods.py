52ee4ab9a88eec39ed695eaa0d7c0d5bddb2b9e5
JinsolKim-1 <atomicnick43@gmail.com>
2023-06-16 21:13:45 +0800

    string methods

---
+++ string methods.py
@@ -0,0 +1,13 @@
+#string methods
+name = "matthewCOLOMBUS"
+
+print (len(name))
+print (name.find ("O"))
+print (name.capitalize ())
+print (name.upper ())
+print (name.lower ())
+print (name.isdigit ())
+print (name.isalpha ())
+print (name.count ("t"))
+print (name.replace("COLOMBUS","mateo"))
+print (name*5)
---
+++ tempCodeRunnerFile.py
@@ -0,0 +1,6 @@
+#mulitple assignment
+
+name,weight,safe = "SCP-049", 92.754, True
+print ("It is " +str(name) +" ")
+print ("It weighs exactly " +str(weight) +" ")
+print ("and is safe. Yes? " +str(safe))
