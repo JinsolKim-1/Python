fc6382b04f75d7dfeb5e1c64da1b509459627ee6
JinsolKim-1 <atomicnick43@gmail.com>
2023-06-18 23:47:47 +0800

    type casts

---
+++ type casts.py
@@ -0,0 +1,23 @@
+#type castings
+
+a = 43
+b = 47.61
+c = "78"
+d = 62.4
+e = "32"
+f = 67
+
+#non-permanent
+print (float(a))
+print (str(b))
+print (int(c)*4)
+
+#permanent-change
+
+d = str (d)
+e = float (e)
+f = int (f)
+
+print (d*2)
+print (e*2)
+print (f*3)
