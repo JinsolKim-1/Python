43a2e38ac6fbbc2ab0942f8a219b6c9a16b094f4
JinsolKim-1 <atomicnick43@gmail.com>
2023-06-20 23:11:46 +0800

    string-slicings

---
+++ string-slicings.py
@@ -0,0 +1,19 @@
+name = "William Shakespeare"
+
+first = name[0:8]
+last = name[8:20]
+random = name[0:20:3]
+reverse = name[::-1]
+print (first)
+print (last)
+print (random)
+print (reverse)
+
+web ="https://twitter.com"
+web2 ="https:// youtube.com"
+
+slice1 = slice(8,-4)
+slice2 = slice(9,-4)
+print (web[slice1])
+print (web2[slice2])
+
