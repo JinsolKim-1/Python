107f2c23a0e5730f98ab2a81f375bcfedb3f6fa3
JinsolKim-1 <atomicnick43@gmail.com>
2023-06-20 15:51:07 +0800

    pet-input

--- pet-input.py
+++ pet-input.py
@@ -1,3 +1,5 @@
+import math
+
 pet = input ("What is your favorite pet? ")
 age = int(input ("What is your pet's age? "))
 weight = float(input ("What is your pet's weight in pounds? "))
@@ -7,5 +9,5 @@ weight = weight / 2.205
 
 print (pet)
 print ("Your pet's age is approximately "+ str(age) + " weeks old")
-print ("Your pet's weight in Kilogram is: " + str(weight) + "Kg")
+print ("Your pet's weight in Kilogram is: " + str(weight) + " or " + str(round(weight)) + " Kg")
 
