c59dfbfe0ae9f6664d336cb3d34215334f520436
JinsolKim-1 <atomicnick43@gmail.com>
2023-06-22 17:37:36 +0800

    if-else-pet-input

--- if-else-pet-input.py
+++ if-else-pet-input.py
@@ -1,18 +1,17 @@
+ 
 pet = str(input("What is your pet? "))
-
-x = "dog"
-y = "cat"
-z = "bird"
-
 age = int(input("What is your pet's age? "))
-weight = float(input("What is your pet's weight? "))
+weight = float(input("What is your pet's weight in pounds? "))
 
 age = age * 52
 weight = weight / 2.205
 
 
-if pet == x or pet == y or pet == z:
+if pet in ["dog" , "cat" , "bird"]:
     print("Your pet's age is approximately " + str(age) + " weeks old")
     print("Your pet's weight in Kilogram is: " + str(weight) + " or " + str(round(weight)) + " Kg")
 else:
-    print("That's an exotic animal!")
+    print("That's an exotic animal you have there!")
+    print("Your pet's age is approximately " + str(age) + " weeks old")
+    print("Your pet's weight in Kilogram is: " + str(weight) + " or " + str(round(weight)) + " Kg")
+    
