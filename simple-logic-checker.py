3c19716dfc6abe620cf82970e4dcf734298b187d
JinsolKim-1 <atomicnick43@gmail.com>
2023-06-22 18:32:51 +0800

    simple-logic-checker

--- simple-logic-checker.py
+++ simple-logic-checker.py
@@ -1,10 +1,12 @@
 num = int(input("Enter the number to check: "))
 lower = int(input("Enter the lower bound: "))
-upper = int (input("Enter the upper bound: "))
+upper = int(input("Enter the upper bound: "))
 
-if num <= upper or num == upper:
+if num <= upper and num >= lower:
     print ("The number is within range!")
-elif num < lower or num >= upper:
-    print ("The number is out of range! ")
+elif num < lower:
+    print ("The number is below the range! ")
+elif num > upper:
+    print ("The number is above the range! ")
 
 k=input(("press ") + ("'ENTER'") + (" to exit"))
