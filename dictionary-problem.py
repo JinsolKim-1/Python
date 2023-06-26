a3c925769a677dbb8f1e8394956a02658704550f
JinsolKim-1 <atomicnick43@gmail.com>
2023-06-26 23:10:48 +0800

    dictionary-problem

--- dictionary-problem.py
+++ dictionary-problem.py
@@ -1,1 +1,14 @@
 #dictionary
+
+num = int(input("Input number of students: "))
+lvl = float(input("Input number of grades: "))
+
+name = str(input("Student's name: "))
+age = int(input("Student's age: "))
+grade = float(input("Student's grade: "))
+
+students = {'name: ': name , 'age: ': age , 'grade: ': grade}
+
+average = num / age 
+average2= grade / lvl           
+print()
